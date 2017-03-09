ff984fb843703f93fb910ea95def0c635572b120
starship9 <nishanksaini10@gmail.com>
2017-03-09 22:43:04 +0530

    Base code for question 3

---
+++ src/toc1washingmachine/ToC1WashingMachine.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toc1washingmachine;
+
+/**
+ *
+ * @author starship9
+ */
+
+public class ToC1WashingMachine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class WashingMachine{
+        
+        int fabricType;
+        int waterTemp;
+        int machineRPM;
+        int stateTime;
+        String currState [] = {"Pre-wash","Wash","Rinse","Spin","Start"};
+        public WashingMachine(){
+            fabricType = 1;
+            waterTemp = 20;
+            machineRPM = 400;
+//            stateTime = 
+        }
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
+
