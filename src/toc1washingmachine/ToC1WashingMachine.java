1e16a74a337a3fd0deec943aa1bd522749c2bd7e
starship9 <nishanksaini10@gmail.com>
2017-03-09 23:14:11 +0530

    Removed default constructor

--- src/toc1washingmachine/ToC1WashingMachine.java
+++ src/toc1washingmachine/ToC1WashingMachine.java
@@ -15,7 +15,7 @@ public class ToC1WashingMachine {
     /**
      * @param args the command line arguments
      */
-    public class WashingMachine{
+    public static class WashingMachine{
         
         int fabricType;
         int waterTemp;
@@ -23,15 +23,15 @@ public class ToC1WashingMachine {
         int stateTime [] = new int[4];
         String currState [] = {"Pre-wash","Wash","Rinse","Spin"};
         boolean startStatus;
-        public WashingMachine(){
-            fabricType = 1;
-            waterTemp = 20;
-            machineRPM = 400;
-            stateTime[0] = 10;
-            stateTime[1] = 18;
-            stateTime[2] = 14;
-            stateTime[3] = 4;
-        }
+//        public WashingMachine(){
+//            fabricType = 1;
+//            waterTemp = 20;
+//            machineRPM = 400;
+//            stateTime[0] = 10;
+//            stateTime[1] = 18;
+//            stateTime[2] = 14;
+//            stateTime[3] = 4;
+//        }
 
         
     }
@@ -40,6 +40,9 @@ public class ToC1WashingMachine {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        WashingMachine wm = new WashingMachine();
+        
+        
     }
     
     
