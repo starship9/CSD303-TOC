8421e0ac182ce4aebdc3f7b264a1d9ed281eb25c
starship9 <nishanksaini10@gmail.com>
2017-03-09 23:07:28 +0530

    Updated machine class

--- src/toc1washingmachine/ToC1WashingMachine.java
+++ src/toc1washingmachine/ToC1WashingMachine.java
@@ -20,14 +20,20 @@ public class ToC1WashingMachine {
         int fabricType;
         int waterTemp;
         int machineRPM;
-        int stateTime;
-        String currState [] = {"Pre-wash","Wash","Rinse","Spin","Start"};
+        int stateTime [] = new int[4];
+        String currState [] = {"Pre-wash","Wash","Rinse","Spin"};
+        boolean startStatus;
         public WashingMachine(){
             fabricType = 1;
             waterTemp = 20;
             machineRPM = 400;
-//            stateTime = 
+            stateTime[0] = 10;
+            stateTime[1] = 18;
+            stateTime[2] = 14;
+            stateTime[3] = 4;
         }
+
+        
     }
     
     
